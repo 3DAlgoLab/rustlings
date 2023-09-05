04791cbf1f93a03bad52616b4c26b2749da3a4ad
3D ALGO LAB <83936830+3DAlgoLab@users.noreply.github.com>
2023-09-05 09:35:41 +0900

    Update lib.rs

--- playgrounds/lib1/src/lib.rs
+++ playgrounds/lib1/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn get_or_default(arg: &Option<String>) -> String {
+    if arg.is_none() {
+        return String::new();
+    }
+    let s = arg.as_ref().unwrap();
+    s.clone()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +18,19 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_gets_or_default() {
+        let result = get_or_default(&None);
+        assert_eq!(result, "");
+        let out = get_or_default(&Some("hello".to_string()));
+        dbg!(out);
+    }
+
+    #[test]
+    fn str_test() {
+        let s = String::from("Hello String");
+        println!("{}", &s[0..3]);
     }
 }
