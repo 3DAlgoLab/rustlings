226bb0f24ba23645146a53799022cc00d74d5c4e
3D ALGO LAB <threealgo@outlook.com>
2023-09-07 16:19:36 +0900

    concurrency

---
+++ playgrounds/concurrency_drill/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use std::os::windows::prelude::HandleOrInvalid;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    println!("Hello, world!");
+    let file_mutex = Arc::new(Mutex::new(
+        OpenOptions::new()
+            .append(true)
+            .write(true)
+            .create(true)
+            .open("increments.txt")
+            .unwrap(),
+    ));
+
+    let mut handles = vec![];
+
+    for i in 0..16 {
+        let file_mutex = Arc::clone(&file_mutex);
+        let handle = thread::spawn(move || {
+            let mut file = file_mutex.lock().unwrap();
+            writeln!(file, "{}", i).unwrap();
+        });
+
+        handles.push(handle);
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
